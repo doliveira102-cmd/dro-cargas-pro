a3e1a2a3d7b308259fa9cc9ea3991c05ea3bb9c6
doliveira102-cmd <doliveira102@gmail.com>
2026-08-12 08:54:21 -0300

    Delete OrdensModule and associated files
    Removed OrdensModule and its related components.

--- backend/src/extras-ordens/ordens_backend/ordens/ordens.module.ts
+++ backend/src/extras-ordens/ordens_backend/ordens/ordens.module.ts
@@ -1,9 +1,1 @@
-import { Module } from '@nestjs/common';
-import { OrdensService } from './ordens.service';
-import { OrdensController } from './ordens.controller';
 
-@Module({
-  providers: [OrdensService],
-  controllers: [OrdensController],
-})
-export class OrdensModule {}
